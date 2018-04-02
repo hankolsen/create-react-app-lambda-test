b38ccf7700950d984a57e83c242aa3cfb676b83e
Hank <henrik@hankolsen.com>
2018-04-02 17:44:12 +0200

    Moved api class

--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -1,6 +1,51 @@
 const fetch = require('node-fetch');
 const slackURL = process.env.SLACK_WEBHOOK_URL;
-const IdentityApi = require('../identity/Identity');
+
+class IdentityApi {
+    constructor(apiURL, token) {
+        this.apiURL = apiURL;
+        this.token = token;
+    }
+
+    headers(headers = {}) {
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.token}`,
+            ...headers
+        };
+    }
+
+    parseJsonResponse(response) {
+        return response.json().then(json => {
+            if (!response.ok) {
+                return Promise.reject({ status: response.status, json });
+            }
+
+            return json;
+        });
+    }
+
+    request(path, options = {}) {
+        const headers = this.headers(options.headers || {});
+        return fetch(this.apiURL + path, { ...options, headers })
+        .then((response) => {
+            const contentType = response.headers.get('Content-Type');
+            if (contentType && contentType.match(/json/)) {
+                return this.parseJsonResponse(response);
+            }
+
+            if (!response.ok) {
+                return response.text().then(data => {
+                    return Promise.reject({ status: response.status, data });
+                });
+            }
+
+            return response.text().then(data => {
+                data;
+            });
+        });
+    }
+}
 
 function fetchUser(identity, id) {
     const api = new IdentityApi(identity.url, identity.token);
